569f7b7cd5d2468fa70f01212abf46c89e668112
Daniela <djimenez57@alumnos.uaq.mx>
2026-09-17 08:49:00 -0600

    Cambios

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Sistema de monitoreo");
     }
-    public static final int LLENADO = 1;// final -> Constante, static -> Todos los valores que puede albergar.
+   // public static final int LLENADO = 1;// final -> Constante, static -> Todos los valores que puede albergar.
     // enum --> A todos los valores los vuelve numéricos, aunque aparezca una palabra.
 }
 //![Bajo la lluvia.png](../Image/Bajo%20la%20lluvia.png) ---> Para agregar imágenes en el doc
